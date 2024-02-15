f7316425b96e4bc84bc4c42e9e1fc4fe68186ad5
Swank <willmadedesign@gmail.com>
2024-02-14 17:25:18 -0800

    Add LoadingPage logic  to display before app loads

--- src/App.js
+++ src/App.js
@@ -5,34 +5,36 @@ import './App.css';
 import './components/styling/LoadingPage.css';
 
 export default function App() {
-  //Need to figure out the loading logic in order to add the component
-  //to load before the main app
-
-  /*const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    //Loading logic goes here
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
-  }, []);*/
+    }, 3000);
+
+    return () => clearTimeout(timer); //Clear the timer
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <LoadingPage />//Render the Loading comp while loading
+      ):(
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      )}
     </div>
   );
 }
