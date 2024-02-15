4021c828ceec718207e5a2ac47877491e9bd6eb9
Swank <willmadedesign@gmail.com>
2024-02-14 17:00:06 -0800

    Add LoadingPage comp to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import LoadingPage from './components/Loader';
 import logo from './logo.svg';
 import './App.css';
+import './components/styling/LoadingPage.css';
 
 export default function App() {
+  //Need to figure out the loading logic in order to add the component
+  //to load before the main app
+
+  /*const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    //Loading logic goes here
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);*/
+
   return (
     <div className="App">
       <header className="App-header">
