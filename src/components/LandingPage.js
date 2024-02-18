f3fb888d1dc4514303fbf0d0e8418f5e81953da4
Swank <willmadedesign@gmail.com>
2024-02-18 15:12:34 -0800

    Build out landing page component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,1 +1,23 @@
-import React from "react";
+import React from 'react';
+import '../styling/global.css';
+
+const LandingPage = () => {
+    return (
+        <div className='landing'>
+            <header className='landing-header'>
+                <h1>JOURNEYMAP</h1>
+                <p>Let's build the journey of a lifetime.</p>
+            </header>
+
+            <div className='landing-interactive'>
+                {/*interactive globe here*/}
+            </div>
+
+            <div className='landing-navigation-cues'>
+
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
