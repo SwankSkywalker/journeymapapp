59ee344af63684cc59deca2fa5c12ec8566d1ad1
Swank <willmadedesign@gmail.com>
2024-02-19 11:21:08 -0800

    Add landingpg component to main app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import LoadingPage from './components/Loader.js';
-import logo from './logo.svg';
+import LandingPage from './components/LandingPage.js';
+import './assets/jfry.jpeg';
 import './styling/global.css';
 
 export default function App() {
@@ -19,20 +20,7 @@ export default function App() {
       {loading ? (
         <LoadingPage />//Render the Loading comp while loading
       ):(
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <LandingPage />
       )}
     </div>
   );
