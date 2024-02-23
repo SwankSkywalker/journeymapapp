dc9e1bbb7b01f9d20c3d0214c385594852bcdbf1
Swank <willmadedesign@gmail.com>
2024-02-23 15:35:27 -0800

    Fix background

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './styling/global.css';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const unlockPosition = { x: 635, y: 555}; //replace with actual lock position
+  const unlockPosition = { x: 635, y: 530}; //replace with actual lock position
 
   useEffect(() => {
     const timer = setTimeout(() => {
